565e311db5e583bee162e2407da7c29692a3e0c3
Skrakenes <kristian.sk@hotmail.com>
2025-12-02 15:30:41 +0100

    Started to cahge PlatformService to use sql server

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -5,28 +5,37 @@ using PlatformService.SyncDataServices.Http;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AppDbContext>(opt =>
-        opt.UseInMemoryDatabase("InMem"));
+
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
+// Configure DbContext depending on environment
+//if (builder.Environment.IsProduction())
+//{
+    Console.WriteLine("--> Using SqlServer Db");
+    builder.Services.AddDbContext<AppDbContext>(opt =>
+        opt.UseSqlServer(builder.Configuration.GetConnectionString("PlatformsConn")));
+//}
+//else
+//{
+  //  Console.WriteLine("--> Using InMem Db");
+    //builder.Services.AddDbContext<AppDbContext>(opt =>
+      //  opt.UseInMemoryDatabase("InMem"));
+//}
 
 var app = builder.Build();
-
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    
 }
 
 app.UseHttpsRedirection();
 
-PrepDb.PrepPopulation(app);
+//PrepDb.PrepPopulation(app, app.Environment.IsProduction());
 app.MapControllers();
 app.Run();
