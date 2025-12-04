875bf3945c25d2df7ba3792dcd99e11442522724
Skrakenes <kristian.sk@hotmail.com>
2025-12-04 15:10:39 +0100

    Data Layer for CommandService, Data, Dto, Models and Profiles

---
+++ CommandsService/Data/ICommandRepo.cs
@@ -0,0 +1,18 @@
+﻿using CommandsService.Models;
+
+namespace CommandsService.Data;
+
+public interface ICommandRepo
+{
+    bool SaveChages();
+
+    //Platforms
+    IEnumerable<Platform> GetAllPlatforms();
+    void CreatePlatform(Platform plat);
+    bool platformExsists(int platformId);
+
+    //Commands
+    IEnumerable<Command> GetCommandsForPlatform(int platformId);
+    Command GetCommand(int platformId, int commandId);
+    void CreateCommand(int platformId, Command command);
+}
---
+++ CommandsService/Dtos/CommandCreateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommandsService.Dtos;
+
+public class CommandCreateDto
+{
+    [Required]
+    public required string HowTo { get; set; }
+    [Required]
+    public required string CommandLine { get; set; }
+}
