ded05c33b0f329d4d0606e07bda6384f6b33bfa4
Kristian Kråkenes <Kristian.sk@hotmail.com>
2026-01-28 15:27:58 +0100

    Add gRPC integration for platform data retrieval and update configurations

--- CommandsService/Program.cs
+++ CommandsService/Program.cs
@@ -1,6 +1,7 @@
 using CommandsService.AsyncDataServices;
 using CommandsService.Data;
 using CommandsService.EventProcessing;
+using CommandsService.SyncDataServices.Grpc;
 using Microsoft.EntityFrameworkCore;
 using System;
 
@@ -11,19 +12,22 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
         opt.UseInMemoryDatabase("InMem"));
 builder.Services.AddScoped<ICommandRepo, CommandRepo>();
 builder.Services.AddControllers();
-builder.Services.AddHostedService<MessageBusSubscriber>();
+
 builder.Services.AddSingleton<IEventProcessor, EventProcessor>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IPlatformDataClient, PlatformDataClient>();
+
+if(builder.Environment.IsProduction())
+{
+        builder.Services.AddHostedService<MessageBusSubscriber>();
+}
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.MapOpenApi();
-//}
 
 app.UseHttpsRedirection();
 
+PrepDb.PrepPopulation(app);
 
 app.MapControllers();
 
@@ -34,22 +38,3 @@ app.Run();
 
 
 
-
-//builder.Services.AddDbContext<AppDbContext>(opt =>
-//        opt.UseInMemoryDatabase("InMem"));
-//builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
-//builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
-
-//var app = builder.Build();
-
-
-
-//// Configure the HTTP request pipeline.
-
-
-//app.UseHttpsRedirection();
-
-//PrepDb.PrepPopulation(app);
-
-//app.Run();
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using PlatformService.AsyncDataServices;
 using PlatformService.Data;
+using PlatformService.SyncDataServices.Grpc;
 using PlatformService.SyncDataServices.Http;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +13,7 @@ builder.Services.AddControllers();
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 builder.Services.AddSingleton<IMessageBusClient, MessageBusClient>();
+builder.Services.AddGrpc();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 //Configure DbContext depending on environment
@@ -33,11 +35,18 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    
+
 }
 
 app.UseHttpsRedirection();
 
 PrepDb.PrepPopulation(app, app.Environment.IsProduction());
+
 app.MapControllers();
+app.MapGrpcService<GrpcPlatformService>();
+app.MapGet("/protos/platforms.proto", async context =>
+{
+    await context.Response.WriteAsync(File.ReadAllText("Protos/platforms.proto"));
+});
+
 app.Run();
